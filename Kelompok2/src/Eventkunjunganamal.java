7f18f9739f235a9c560d56a441bd701255db7aee
owenfelix04 <owen.felix04@gmail.com>
2023-05-27 22:10:02 +0700

    Update Eventkunjunganamal.java

---
+++ Kelompok2/src/Eventkunjunganamal.java
@@ -0,0 +1,6 @@
+// Pada kelas ini akan mengeluarkan output lokasi penyelenggaraan event, waktu, dan tanggal
+// Data yang di keluarkan pada output akan mengarahkan pengguna untuk memilih input
+// Dalam class ini bakal mengarahkan pengguna untuk menginput event apa yang akan di pilih
+public class Eventkunjunganamal {
+    
+}
