004a986a9e3d6cb7e40d481701acba3a91e63ddb
owenfelix04 <owen.felix04@gmail.com>
2023-05-27 22:10:30 +0700

    Update Rating.java

--- Kelompok2/src/Rating.java
+++ Kelompok2/src/Rating.java
@@ -1,3 +1,6 @@
 // Pada kelas ini akan mengarahkan pengguna untuk memberikan nilai kepada app ini
 // Data yang terdapat di dalam akan mengarahakn user untuk menginput data user rating
 // Dalam class ini bakal digunakan untuk mengarahkan pengguna untuk memberikan nilai kepada app ini
+public class Rating {
+    
+}
