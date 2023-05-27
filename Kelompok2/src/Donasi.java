5d329981525439283200a9f95447a2cf1f1c1d9a
AudreyTamalate <134504764+AudreyTamalate@users.noreply.github.com>
2023-05-27 15:14:29 +0700

    isi donasi
    isi donasi

---
+++ Kelompok2/src/Donasi.java
@@ -0,0 +1,6 @@
+//Pada kelas ini akan menampung jenis donasi dan siapa yang memberi donasi tersebut
+// Data yang ditampung berupa jenis donasi, nama donatur, jumlah donasi, nomor telepon donatur
+//Kelas ini akan digunakan untuk membantu semua kegiatan yang akan dilaksanakan pada Tzu Chi
+public class Donasi {
+    
+}
