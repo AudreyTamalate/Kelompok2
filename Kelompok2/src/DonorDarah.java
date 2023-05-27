da770293e24361912558076976c816bc9e57785c
JESSLYNP1511 <jesslynyoman@gmail.com>
2023-05-27 15:27:38 +0700

    isi Donor Darah
    isi Donor Darah

---
+++ Kelompok2/src/DonorDarah.java
@@ -0,0 +1,6 @@
+//Kelas ini menampung data Pendonor Darah
+//Data yang ditampung berupa Nama Pendonor, Jenis Kelamin Pendonor, Golongan Darah Pendonor, dan Keberhasilan cek darah.
+//Kelas ini digunakan untuk mengumpulkan darah dari para pendonor untuk membantu orang yang membutuhkan
+public class DonorDarah {
+    
+}
