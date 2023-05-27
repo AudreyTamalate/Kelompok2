0e202d0942798bb50bbbaf4e47a6546967d9c9bc
WinnieFelicia <winnie.feliciaa@gmail.com>
2023-05-27 15:20:21 +0700

    isi daur ulang
    isi daur ulang

---
+++ Kelompok2/src/DaurUlang.java
@@ -0,0 +1,6 @@
+//Pada kelas ini akan menampung jenis barang bekas, jumlah barang bekas, nama dari orang yang mengikuti daur ulang
+//Data yang ditampung berupa jenis barang bekas, jumlah barang bekas, dan nama dari yang mengikuti daur ulang
+//Kelas ini akan digunakan untuk membantu Tzu Chi mendapatkan barang bekas dalam melakukan daur ulang
+public class DaurUlang {
+    
+}
