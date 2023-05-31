7a9077648f7744876c01aea3588ab4f5c460e4c0
owenfelix04 <owen.felix04@gmail.com>
2023-05-31 20:30:59 +0700

    sesi 4
    update sesi 4

--- Kelompok2/src/Eventkunjunganamal.java
+++ Kelompok2/src/Eventkunjunganamal.java
@@ -1,6 +1,35 @@
 // Pada kelas ini akan mengeluarkan output lokasi penyelenggaraan event, waktu, dan tanggal
 // Data yang di keluarkan pada output akan mengarahkan pengguna untuk memilih input
 // Dalam class ini bakal mengarahkan pengguna untuk menginput event apa yang akan di pilih
+//owen(03081220010)
 public class Eventkunjunganamal {
-    
+    private String NamaEvent;
+    private String Waktu;
+    private String Tanggal;
+
+
+    public String getNamaEvent() {
+        return this.NamaEvent;
+    }
+
+    public void setNamaEvent(String NamaEvent) {
+        this.NamaEvent = NamaEvent;
+    }
+
+    public String getWaktu() {
+        return this.Waktu;
+    }
+
+    public void setWaktu(String Waktu) {
+        this.Waktu = Waktu;
+    }
+
+    public String getTanggal() {
+        return this.Tanggal;
+    }
+
+    public void setTanggal(String Tanggal) {
+        this.Tanggal = Tanggal;
+    }
+
 }
--- Kelompok2/src/Rating.java
+++ Kelompok2/src/Rating.java
@@ -1,6 +1,35 @@
 // Pada kelas ini akan mengarahkan pengguna untuk memberikan nilai kepada app ini
 // Data yang terdapat di dalam akan mengarahakn user untuk menginput data user rating
 // Dalam class ini bakal digunakan untuk mengarahkan pengguna untuk memberikan nilai kepada app ini
+//owen(03081220010)
 public class Rating {
-    
+    private String Nama;
+    private String NIK;
+    private String RatingScore;
+
+
+    public String getNama() {
+        return this.Nama;
+    }
+
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+
+    public String getNIK() {
+        return this.NIK;
+    }
+
+    public void setNIK(String NIK) {
+        this.NIK = NIK;
+    }
+
+    public String getRatingScore() {
+        return this.RatingScore;
+    }
+
+    public void setRatingScore(String RatingScore) {
+        this.RatingScore = RatingScore;
+    }
+
 }
