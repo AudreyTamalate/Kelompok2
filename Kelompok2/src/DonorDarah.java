0ea0e7d2b87ab0cba638b5d8245a8918113e8e14
JESSLYNP1511 <jesslynyoman@gmail.com>
2023-05-31 20:21:52 +0700

    Setter Getter Donor Darah
    Setter Getter Donor Darah

--- Kelompok2/src/DonorDarah.java
+++ Kelompok2/src/DonorDarah.java
@@ -2,5 +2,45 @@
 //Data yang ditampung berupa Nama Pendonor, Jenis Kelamin Pendonor, Golongan Darah Pendonor, dan Keberhasilan cek darah.
 //Kelas ini digunakan untuk mengumpulkan darah dari para pendonor untuk membantu orang yang membutuhkan
 public class DonorDarah {
-    
+<<<<<<< HEAD
+    private String NamaPendonor;
+    private String JenisKelaminPendonor;
+    private String GolonganDarahPendonor;
+    private String KeberhasilanCekDarah;
+
+
+    public String getNamaPendonor() {
+        return this.NamaPendonor;
+    }
+
+    public void setNamaPendonor(String NamaPendonor) {
+        this.NamaPendonor = NamaPendonor;
+    }
+
+    public String getJenisKelaminPendonor() {
+        return this.JenisKelaminPendonor;
+    }
+
+    public void setJenisKelaminPendonor(String JenisKelaminPendonor) {
+        this.JenisKelaminPendonor = JenisKelaminPendonor;
+    }
+
+    public String getGolonganDarahPendonor() {
+        return this.GolonganDarahPendonor;
+    }
+
+    public void setGolonganDarahPendonor(String GolonganDarahPendonor) {
+        this.GolonganDarahPendonor = GolonganDarahPendonor;
+    }
+
+    public String getKeberhasilanCekDarah() {
+        return this.KeberhasilanCekDarah;
+    }
+
+    public void setKeberhasilanCekDarah(String KeberhasilanCekDarah) {
+        this.KeberhasilanCekDarah = KeberhasilanCekDarah;
+    }
+=======
+
+>>>>>>> e30ff747629f23343714d7a9292e6113b433faa2
 }
