eb8d75a2af72cea6dd95f795655384d132de31cd
WinnieFelicia <winnie.feliciaa@gmail.com>
2023-06-05 20:59:48 +0700

    winnie sesi 5 init
    sesi5init

--- Kelompok2/src/App.java
+++ Kelompok2/src/App.java
@@ -58,6 +58,7 @@ public class App {
     }
 
     public static void init(){
+<<<<<<< Updated upstream
         Rating budi = new Rating("Budi", "1854211", "2.5");
         Rating anto = new Rating("Anto", "15151110", " 3.8");
         Rating gopal = new Rating("gopal", "18415153", "4.8");
@@ -67,6 +68,17 @@ public class App {
         Eventkunjunganamal budi = new Eventkunjunganamal("Donor Darah", "12.00", "01 April 2023");
         Eventkunjunganamal anto = new Eventkunjunganamal("Bakti Sosial", "13.00", "02 Mei 2023");
         Eventkunjunganamal gopal = new Eventkunjunganamal("Ulang Tahun Yayasan", "11.00", "05 Januari 2023");
+=======
+        DaurUlang anita = new DaurUlang("Sedotan", "20", "Anita");
+        DaurUlang iwan = new DaurUlang("Kaleng", "10", "Iwan");
+        DaurUlang sally = new DaurUlang("Botol Aqua", "5", "Sally");
+
+    public static void init(){
+        Donasi Ipin = new Donasi("Barang", "Ipin", "-", "2", "082345670987");
+        Donasi Mail = new Donasi("Dana", "Mail", "Rp. 100.000", "-", "082546789345");
+        Donasi Rose = new Donasi("Dana", "Rose", "Rp. 500.000", "-", "0812345678909");
+
+>>>>>>> Stashed changes
     }
 }
 
