e11c2023c7d252d84b34d1396e3dae2e19b1332d
owenfelix04 <owen.felix04@gmail.com>
2023-06-07 22:56:57 +0700

    Update init 07/06/2023
    init latest 07/06/2023

--- Kelompok2/src/App.java
+++ Kelompok2/src/App.java
@@ -69,12 +69,13 @@ public class App {
         Eventkunjunganamal anto = new Eventkunjunganamal("Bakti Sosial", "13.00", "02 Mei 2023");
         Eventkunjunganamal gopal = new Eventkunjunganamal("Ulang Tahun Yayasan", "11.00", "05 Januari 2023");
     }
-    Public static void init(){
+
+    public static void init(){
         DaurUlang anita = new DaurUlang("Sedotan", "20", "Anita");
         DaurUlang iwan = new DaurUlang("Kaleng", "10", "Iwan");
         DaurUlang sally = new DaurUlang("Botol Aqua", "5", "Sally");
     }
-        
+
     public static void init(){
         Donasi Ipin = new Donasi("Barang", "Ipin", "-", "2", "082345670987");
         Donasi Mail = new Donasi("Dana", "Mail", "Rp. 100.000", "-", "082546789345");
@@ -85,6 +86,12 @@ public class App {
         DonorDarah usman = new DonorDarah("Usman Halim", "Laki-laki", "O", "Berhasil");
         DonorDarah susiyanti = new DonorDarah("Susiyanti Tambunan", "Perempuan", "A+", "Gagal");
         DonorDarah susmanto = new DonorDarah("Susmanto Amin", "Laki-laki", "B+", "Berhasil");
+    }
+
+    public static void init(){
+        Relawan Upin = new Relawan("Upin", "18", "1201008186030062", "Laki-laki", "0857983216521");
+        Relawan Ehsan = new Relawan("Ehsan", "12", "120451515860151", "Laki-laki", "0845626295164");
+        Relawan jennie = new Relawan("Jennie", "22", "1208151315161315315", "Perempuan", "081511161");
     }
 }
 
